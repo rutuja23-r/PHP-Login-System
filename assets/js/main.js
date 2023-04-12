90fe219a567ea6818af256f6d635e1d3f70da5fa
rutuja23-r <rutujamete2323@gmail.com>
2023-04-12 16:52:14 +0530

    modified:   assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,19 @@ $(document)
         var _form = $(this) ;
         var _error = $(".js-error" ,_form) ;
 
-        var data = {
+        var dataObj = {
                email:$("input[type='email']" , _form).val() ,
             password: $("input[type='password']" , _form).val()
         } ;
 
-        if(data.email.length < 6) {
+        if(dataObj.email.length < 6) {
             _error 
             .text("please enter a valid email address ")
             .show();
             return false ;
 
         }
-        else if (data.password.length < 11) {
+        else if (dataObj.password.length < 11) {
             _error
             .text("please enter a password that is at least 11 characters long ")
             .show();
@@ -25,7 +25,20 @@ $(document)
 
         }
         // ajax code started 
-        _error.hide()
+        _error.hide() ;
+
+        $.ajax({
+            type :'POST' ,
+            url : '/' ,
+            data :dataObj ,
+            dataType : 'json' ,
+            async :true ,
+        })
+
+
+
+
+        
         
 
     
